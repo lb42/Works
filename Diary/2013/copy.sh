0a817cd5e0cee63f2fdf0ea83d1826650ed9fa18
lb42 <lb42@github.com>
2026-05-11 13:29:10 +0100

    lots of blogs

---
+++ Diary/2013/copy.sh
@@ -0,0 +1,5 @@
+cp ~/Public/Works/Diary/Wordpress/media/2013/04/20130409_121332.jpg media
+cp ~/Public/Works/Diary/Wordpress/media/2013/04/20130409_153849_v1.jpg media
+cp ~/Public/Works/Diary/Wordpress/media/2013/04/20130410_105105.jpg media
+cp ~/Public/Works/Diary/Wordpress/media/2013/04/20130410_201617.jpg media
+
